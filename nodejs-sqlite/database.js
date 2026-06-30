788b0c53b7d6116372de5096c7523e08bd6e720b
Naiiden <hugo.plissonneau@viacesi.fr>
2026-06-30 10:40:59 +0200

    add infrastructure for backend setup

--- nodejs-sqlite/database.js
+++ nodejs-sqlite/database.js
@@ -4,13 +4,13 @@ const db = new DatabaseSync('data.db')
 db.exec(`
   CREATE TABLE IF NOT EXISTS history(
     id INTEGER PRIMARY KEY AUTOINCREMENT,
-    url TEXT,
-);     
+    url TEXT
+  );     
     CREATE TABLE IF NOT EXISTS bookmarks(
     id INTEGER PRIMARY KEY AUTOINCREMENT,
     url TEXT,
-    date_added DATETIME DEFAULT CURRENT_TIMESTAMP,
-); 
+    date_added DATETIME DEFAULT CURRENT_TIMESTAMP
+  ); 
 `)
 
 module.exports = db;
--- nodejs-sqlite/index.js
+++ nodejs-sqlite/index.js
@@ -1,3 +1,26 @@
+const express = require('express')
+const cors = require('cors')
 const db = require('./database')
+
+const app = express()
+
+app.use(cors())
+app.use(express.json())
+
+
 console.log("Node.js + SQLite");
 console.log(db.prepare("SELECT name FROM sqlite_master WHERE type='table'").all())
+
+app.get('/history', (req, res) =>  {
+    console.log('GET history')
+    
+})
+
+app.post('/history', (req, res) => {
+    console.log('PUT history')
+
+})
+
+
+
+app.listen(8000, () => console.log('Your server is runnning on port 8000'))
